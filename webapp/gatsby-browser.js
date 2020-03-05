0ce3160f6c49b0ac81fe0947cc3af942d5fca748
Marek Dano <mk.dano@gmail.com>
2020-03-05 22:46:02 +0000

    Add auth0 authentication to the webapp.

--- webapp/gatsby-browser.js
+++ webapp/gatsby-browser.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import { navigate } from 'gatsby'
+import { AuthProvider } from 'react-use-auth'
+
+export const wrapRootElement = ({ element }) => (
+	<AuthProvider navigate={navigate} 
+		auth0_domain="noteboard.eu.auth0.com"
+		auth0_client_id="VY59LNL6GmSyrehAXlJsZ8zTIkWK5Mmi"
+	>
+		{element}
+	</AuthProvider>
+)
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,14 +1,31 @@
 import React from "react"
+import { useAuth } from 'react-use-auth'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="noteboard" />
-    <p>Stick your note on the board.</p>
-    <button>Get started</button>
-  </Layout>
-)
+const IndexPage = () => {
+  const { isAuthenticated, user, login, logout } = useAuth()
+  console.table(isAuthenticated(), user)
+
+  return (
+    <Layout>
+      <SEO title="noteboard" />
+      {isAuthenticated() ? 
+        <>
+          <button onClick={logout}>Logout</button>
+          <p>Hello, { user.name }</p>
+        </> : 
+        <>
+          <button onClick={login}>Login</button>
+          <p>Hello, everybody</p>
+        </>
+      }
+      <p>Stick your ordinary note on the board.</p>
+
+      <h5>No notes yet</h5>
+    </Layout>
+  )
+}
 
 export default IndexPage
