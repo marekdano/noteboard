0fa2526100b4952a2ba0825439df1e93dfbfa802
Marek Dano <mk.dano@gmail.com>
2020-03-07 07:42:03 +0000

    Create or update a user when it's logged in the app.

--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useAuth } from 'react-use-auth'
-import { useQuery } from 'react-apollo-hooks'
+import { useQuery, useMutation } from 'react-apollo-hooks'
 import gql from 'graphql-tag'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => {
-  const { isAuthenticated, user, login, logout } = useAuth()
+const LoginButton = () => {
+  const { isAuthenticated, user, userId, login, logout } = useAuth()
 
-  const { data, loading } = useQuery(gql`
+  const [updateUser, { data }] = useMutation(
+    gql`
+      mutation updateUser($userId: String) {
+        updateUser(userId: $userId) {
+          userId
+        }
+      }
+    `,
+    {
+      variables: {
+        userId
+      }
+    }
+  )
+
+  useEffect(() => {
+    userId && updateUser()
+  }, [userId])
+  
+  return isAuthenticated() ? 
+    <>
+      <button onClick={logout}>Logout</button>
+      <p>Hello, { user.name }</p>
+    </> : 
+    <>
+      <button onClick={login}>Login</button>
+      <p>Hello, everybody</p>
+    </>
+}
+
+const IndexPage = () => {
+  const { data } = useQuery(gql`
     query {
       notes {
         content
@@ -20,21 +51,12 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="noteboard" />
-      {isAuthenticated() ? 
-        <>
-          <button onClick={logout}>Logout</button>
-          <p>Hello, { user.name }</p>
-        </> : 
-        <>
-          <button onClick={login}>Login</button>
-          <p>Hello, everybody</p>
-        </>
-      }
+      <LoginButton />
       <p>Stick your ordinary note on the board.</p>
 
       { data && data.notes && data.notes.length ?
         <ul>
-          { data.notes.map(note => (<li>{note.content}</li>)) }
+          { data.notes.map((note, index) => (<li key={index}>{note.content}</li>)) }
         </ul> :
         <h5>No notes yet</h5>
       }
